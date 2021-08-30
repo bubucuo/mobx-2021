c178db6856b99cd5f130910e082a35b5252e9bdb
bubucuo <276185876@qq.com>
2021-08-30 16:53:22 +0800

    innit ls1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import { observer } from "mobx-react-lite";
-import timer from "./store/timer";
-
-export default observer(function App(props) {
+export default function App(props) {
   return (
     <div>
       <h3>App</h3>
-      <button onClick={() => timer.add()}>{timer.sec}</button>
     </div>
   );
-});
+}
